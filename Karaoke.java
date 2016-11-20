f2dc3ee52af2e6519c567f93b58cd86f22ca146e
pownallk <pownallk@gmail.com>
2016-11-20 14:58:02 -0600

    custom serialization

--- Karaoke.java
+++ Karaoke.java
@@ -6,7 +6,11 @@ public class Karaoke{
 
   public static void main (String[] args){
     Songbook songbook = new Songbook();
+    songbook.importFrom("songs.txt");
     KaraokeMachine machine = new KaraokeMachine(songbook);
     machine.run();
+    System.out.println("Saving...");
+    songbook.exportTo("songs.txt");
+    
   }
 }
--- com/teamtreehouse/model/Songbook.java
+++ com/teamtreehouse/model/Songbook.java
@@ -1,16 +1,54 @@
 package com.teamtreehouse.model;
 
+import java.io.*;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+
 
 public class Songbook{
   private List<Song> mSongs;
 
   public Songbook(){
     mSongs = new ArrayList<Song>();
+  }
+
+  public void exportTo(String fileName){
+    try(
+      FileOutputStream fos = new FileOutputStream(fileName);
+      PrintWriter writer = new PrintWriter(fos);
+    ){
+      for(Song song : mSongs){
+        writer.printf("%s|%s|%s%n",
+                      song.getArtist(),
+                      song.getTitle(),
+                      song.getVideoUrl());
+      }
+    } catch(IOException ioe){
+      System.out.printf("Problem saving %s%n", fileName);
+      ioe.printStackTrace();
+    }
+  }
+
+  public void importFrom(String fileName){
+    try(
+      FileInputStream fis = new FileInputStream(fileName);
+      BufferedReader reader = new BufferedReader(new InputStreamReader (fis));
+    ){
+      String line;
+      while((line = reader.readLine()) != null){
+        String[] args = line.split("\\|");
+        addSong(new Song(args[0], args[1], args[2]));
+      }
+    } catch (IOException ioe){
+      System.out.printf("Problem loading %s%n", fileName);
+      ioe.printStackTrace();
+    }
   }
 
   public void addSong (Song song){
@@ -23,7 +61,7 @@ public class Songbook{
 
   //FIXME: this should be cached!!
   private Map<String, List<Song>> byArtist(){
-    Map<String, List<Song>> byArtist = new HashMap<>();
+    Map<String, List<Song>> byArtist = new TreeMap<>();
     for(Song song : mSongs){
       List<Song> artistSongs = byArtist.get(song.getArtist());
       if(artistSongs == null){
@@ -40,6 +78,16 @@ public class Songbook{
   }
 
   public List<Song> getSongsForArtist(String artistName){
-    return byArtist().get(artistName);
+    List<Song> songs = byArtist().get(artistName);
+    songs.sort(new Comparator<Song>(){
+      @Override
+      public int compare(Song song1, Song song2){
+        if(song1.equals(song2)){
+          return 0;
+        }
+        return song1.mTitle.compareTo(song2.mTitle);
+      }
+    });
+    return songs;
   }
 }
